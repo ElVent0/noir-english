a77c43e92d1b56a53cda0b8b3b587b9cfc6914f8
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-09-13 21:49:28 +0300

    init

---
+++ readme.md
@@ -0,0 +1,5 @@
+npm i
+npm start
+npm run build-dev
+npm run build-prod
+npm run clear
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import "./index.html";
+import "./sass/main.scss";
+// import "./styles/index.css";
+
+import { something } from "./modules/module";
