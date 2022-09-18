703c2a18ad225e92905b991352c203a70eb4e0c4
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-09-18 20:43:14 +0300

    notiflix added

--- src/signin.js
+++ src/signin.js
@@ -1,10 +1,20 @@
 import "./index.html";
+// import Notiflix from "notiflix";
+import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const signInEl = document.querySelector("[data-sign-in]");
 const signUpEl = document.querySelector("[data-sign-up]");
 const signUpButton = document.querySelector(".sign-in__sign-up");
 const backButton = document.querySelector(".back__paragraph");
 const guestButton = document.querySelector(".sign-in__guest");
+const signInButton = document.querySelectorAll(".sign-in__button");
+
+signInButton.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    Notify.failure("Registration is currently unavailable");
+  });
+});
 
 // ------------------------------------------------------------------
 
